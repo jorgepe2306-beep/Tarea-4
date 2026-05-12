0d7e3f2eb80adc02206580f7c50fc682748e88a7
jorgepe2306-beep <jorgepe2306@gmail.com>
2026-05-12 10:19:02 -0500

    Implementación inicial clase Cliente

--- Ejercicio.py
+++ Ejercicio.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 import logging
-
+# Sistema desarrollado para Software FJ
 # ==========================================
 # CONFIGURACIÓN DE LOGS
 # ==========================================
